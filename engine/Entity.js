27e18e25ac0ae372fdf1c43f70d99d8ab88f0903
BielMaxBR <gbielmaximo@gmail.com>
2021-04-24 20:02:50 -0300

    um objeto que se mexe

--- engine/Entity.js
+++ engine/Entity.js
@@ -1,9 +1,19 @@
 import Vector from "./Vector.js"
 
 export default function(){
-    this.location = new Vector(0,0)
+    this.position = new Vector(0,0)
+    
+    this.velocity = new Vector(0,0)
+    
+    this.aceleration = new Vector(0,0)
+    
     this.draw = (ctx) => {
         ctx.fillStyle = '#ffffff'
-        ctx.fillRect(this.location.x,this.location.y,10,10)
+        ctx.fillRect(this.position.x,this.position.y,10,10)
+    }
+    
+    this.update = () => {
+      this.velocity.add(this.aceleration)
+      this.position.add(this.velocity)
     }
 }
--- index.js
+++ index.js
@@ -12,15 +12,23 @@ const obj = new engine.Entity()
 
 function Setup() {
   let v1 = new Vector(3,3)
-  let n1= 2
+  let n1 = 2
   v1.div(n1)
   console.log(v1.toArray())
 }
 
 
 function Update(delta) {
+  this.ctx.clearRect(0,0,this.width,this.height)
+  
+  obj.velocity.x = 1
+  
+  obj.update()
   obj.draw(this.ctx)
-  obj.location.x += 1
+  
+  if(obj.position.x > this.width) {
+    obj.position.x = -10
+  }
 }
 
 engine.init()
