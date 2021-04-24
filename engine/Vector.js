6b873fc15e559d5dc4d88e405aa8986f1537644d
BielMaxBR <gbielmaximo@gmail.com>
2021-04-24 12:51:54 -0300

    update na classe vector

--- engine/Vector.js
+++ engine/Vector.js
@@ -1,29 +1,42 @@
-export default function(x,y) {
+export default function Vector(x,y) {
     this.x = x ? x : 0
     this.y = y ? y : 0
     
-    this.add = function(Vector) {
+    this.add = (Vector) => {
       this.x += Vector.x
       this.y += Vector.y 
     }
     
-    this.sub = function(Vector) {
+    this.sub = (Vector) => {
       this.x -= Vector.x
       this.y -= Vector.y
     }
     
     
-    this.mult = function(Vector) {
-      this.x *= Vector.x
-      this.y *= Vector.y
+    this.mult = (number) => {
+      this.x *= number
+      this.y *= number
     }
     
-    this.div = function(Vector) {
-      this.x /= Vector.x
-      this.y /= Vector.y
+    this.div = (number) => {
+      this.x /= number
+      this.y /= number
     }
     
-    this.toArray = function() {
+    this.mag = () => {
+      return Math.sqrt(
+          this.x ** 2 + this.y ** 2
+        )
+    }
+    
+    this.normalized = () => {
+      const mag = this.mag()
+      if (mag > 0) {
+        this.div(mag)
+      }
+    }
+    
+    this.toArray = () => {
       return {
         x: this.x,
         y: this.y
