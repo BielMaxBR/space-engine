f27914fb63d15b30b8be0037bf3494373fcca7c3
Gabriel Maximo <gbielmaximo@gmail.com>
2021-04-25 18:38:46 +0000

    tirado espaços

--- engine/Vector.js
+++ engine/Vector.js
@@ -8,33 +8,27 @@ export default class Vector {
     this.x += v.x
     this.y += v.y 
   }
-  
   sub(v) {
     this.x -= v.x
     this.y -= v.y
   }
-      
   scale(n) {
     this.x *= n
     this.y *= n
   }
-      
   div(n) {
     this.x /= n
     this.y /= n
   }
-      
   length() {
     return Math.sqrt(this.x ** 2 + this.y ** 2)
   }
-      
   normalized() {
     let len = this.length()
     if (len > 0) {
       this.scale(1 / len)
     }
   }
-      
   limit(s) {
     let len = this.length()
         
@@ -42,23 +36,19 @@ export default class Vector {
       this.scale(s / len)
     }
   }
-      
   lerp(v, t) {
     this.x = this.x + (v.x - this.x) * t
     this.y = this.y + (v.y - this.y) * t
   }
-    
   toObject() {
     return {
       x: this.x,
       y: this.y
     }
   }
-      
   toArray() {
     return [this.x, this.y]
   }
-  
   test() {
     return 3;
   }
