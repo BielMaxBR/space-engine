19a3eb578219b4ab3a9506e6455a1ee72441cfd7
BielMaxBR <gbielmaximo@gmail.com>
2021-04-26 22:33:23 -0300

    sistema solar

--- index.js
+++ index.js
@@ -11,40 +11,53 @@ const engine = new Engine({
 
 const Vector = engine.Vector
 
-let planeta1 = new Planet({
-  tam: 10,
+let terra = new Planet({
+  tam: 15,
   mass: 30,
-  color: "rgb(200, 200, 200)"
+  color: "rgb(100, 150, 200)"
 })
 
-let planeta2 = new Planet({
-  tam: 20,
+let sol = new Planet({
+  tam: 40,
   mass: 130,
-  color: "rgb(240, 240, 240)"
+  color: "yellow"
 })
 
+let lua = new Planet({
+  tam: 5,
+  mass: 10,
+  color: "#aaaaaa"
+})
 
 function Setup() {
-  planeta1.position = new Vector(250, 100)
-  planeta2.position = new Vector(250, 250)
-  planeta1.velocity.x = 2
-
-  planeta1.G = 10
-  planeta2.G = 10
+  terra.position = new Vector(250, 100)
+  sol.position = new Vector(250, 250)
+  lua.position = new Vector(250, 50)
   
-  this.append(planeta1)
-  this.append(planeta2)
+  terra.velocity.x = 3
+  lua.velocity.x = 5
+  
+  terra.G = 12
+  sol.G = 10
+  
+  this.append(terra)
+  this.append(sol)
+  this.append(lua)
 }
 
 
 function Update(delta) {
 
-  let f2to1 = planeta2.calculateAttraction(planeta1)
+  let f1 = sol.calculateAttraction(terra)
   
-  planeta1.applyForce(f2to1)
+  let f2 = terra.calculateAttraction(lua)
   
-  planeta1.update()
-  planeta2.update()
+  terra.applyForce(f1)
+  lua.applyForce(f2)
+  
+  terra.update()
+  sol.update()
+  lua.update()
 }
 
 
